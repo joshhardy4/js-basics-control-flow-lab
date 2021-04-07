f9bd6ea683b5ddfc3fe5d202eb11eb3cbe114217
Josh <leonidus00@yahoo.com>
2021-04-07 14:13:55 -0500

    commit tree 8f90d764d4aa12356e1d7c88d854628777a92a54

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+const { __esModule } = require("babel-core");
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride = 199){
+  
+ if (ride <= 400) {
+   return("This one is on me!");
+ } else if (ride > 1999  &&  ride < 2500) {
+   return ("I will gladly take your thirty bucks.");
+  
+  }  if (ride > 2500) {
+   return ("No can do.");
+  }
+  }
+  
+ 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+function ternaryCheckCity(city){
+
+   if (city == 'NYC') {
+    return ('Ok, sounds good.');
+   
+    } else {    
+          return ("No go.");
+    }
+   
+  }
+
+function switchOnCharmFromTip(tip){
+ 
+if (tip === 'generous') 
+return ('Thank you so much.');
+ if (tip === 'not as generous')
+ return ('Thank you.');
+ else {
+   return ('Bye.')
+ }
+   
+ }
+ 
+
+
+ 
